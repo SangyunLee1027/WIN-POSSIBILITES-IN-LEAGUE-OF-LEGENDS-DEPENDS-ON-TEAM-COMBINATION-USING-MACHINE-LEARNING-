d2f63bdff79ba3a1f01700a22e44e30dd0f7e0e1
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 22:27:50 -0500

    datamining

--- src/DataMining.py
+++ src/DataMining.py
@@ -1,5 +1,75 @@
 import pandas as pd
+from riotwatcher import LolWatcher, ApiError
 
 
-def datamining():
-    return
+def arrdata(m):
+    tempdata = {}
+    tempdata['top1'] = ''
+    tempdata['jungle1'] = ''
+    tempdata['mid1'] = ''
+    tempdata['bottom1'] = ''
+    tempdata['support1'] = ''
+    tempdata['top2'] = ''
+    tempdata['jungle2'] = ''
+    tempdata['mid2'] = ''
+    tempdata['bottom2'] = ''
+    tempdata['support2'] = ''
+    win = False
+    for i in range(10):
+        if m['info']['participants'][i]['individualPosition'] == 'TOP':
+            if m['info']['participants'][i]['teamId'] == 100:
+                tempdata['top1'] = m['info']['participants'][i]['championName']
+                if m['info']['participants'][i]['win'] == True:
+                    win = True
+            else:
+                tempdata['top2'] = m['info']['participants'][i]['championName']
+        if m['info']['participants'][i]['individualPosition'] == 'JUNGLE':
+            if m['info']['participants'][i]['teamId'] == 100:
+                tempdata['jungle1'] = m['info']['participants'][i]['championName']
+            else:
+                tempdata['jungle2'] = m['info']['participants'][i]['championName']
+        if m['info']['participants'][i]['individualPosition'] == 'MIDDLE':
+            if m['info']['participants'][i]['teamId'] == 100:
+                tempdata['mid1'] = m['info']['participants'][i]['championName']
+            else:
+                tempdata['mid2'] = m['info']['participants'][i]['championName']
+        if m['info']['participants'][i]['individualPosition'] == 'BOTTOM':
+            if m['info']['participants'][i]['teamId'] == 100:
+                tempdata['bottom1'] = m['info']['participants'][i]['championName']
+            else:
+                tempdata['bottom2'] = m['info']['participants'][i]['championName']
+        if m['info']['participants'][i]['individualPosition'] == 'UTILITY':
+            if m['info']['participants'][i]['teamId'] == 100:
+                tempdata['support1'] = m['info']['participants'][i]['championName']
+            else:
+                tempdata['support2'] = m['info']['participants'][i]['championName']
+    if win == True:
+        tempdata['win'] = 1;
+    else:
+        tempdata['win'] = 0;
+    return tempdata
+
+
+api_key = 'RGAPI-732f6507-1cbd-4822-a5ca-4c44e9dcfa47'
+watcher = LolWatcher(api_key)
+my_region = 'na1'
+me = watcher.summoner.by_name(my_region, 'Skullblue')
+my_matches = watcher.match.matchlist_by_puuid(my_region, me['puuid'])
+
+data = []
+
+# last_match = my_matches[0]
+# match_detail = watcher.match.by_id(my_region, last_match)
+# print(match_detail['info'])
+
+for c in range(10):
+    last_match = my_matches[c]
+    match_detail = watcher.match.by_id(my_region, last_match)
+    if match_detail['info']['gameMode'] == 'CLASSIC':
+        data.append(arrdata(match_detail))
+
+
+
+df = pd.DataFrame(data)
+print(df)
+
